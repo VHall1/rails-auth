be6713f0707dc5e7527d5e22385b36de4d603443
VHall1 <victorhallpsn@hotmail.com>
2023-04-11 12:10:25 +0100

    fix: update column name for username validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   before_save :downcase_email
 
-  validates :name, presence: true, length: { maximum: 50 }
+  validates :username, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
                     uniqueness: true
