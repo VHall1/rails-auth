01a37c6898ba3a96fcc2a8e59161237cf7202c48
VHall1 <victorhallpsn@hotmail.com>
2023-04-11 20:15:22 +0100

    feat: stub auth controller

---
+++ app/controllers/auth_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+  before_action :require_admin!
+
+  def register
+    @user = User.new(user_params)
+
+    if @user.save
+      render json: @user, status: :created, location: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  def login
+    @user = User.find_by(email: params[:email])
+
+    # TODO: Return token instead of user
+    if @user && @user.authenticate(params[:password])
+      render json: @user
+    else
+      render json: { error: 'Invalid username or password' }, status: :unauthorized
+    end
+  end
+
+  private
+
+  # Only allow a list of trusted parameters through.
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+end
