16995b125ada5a13b507bc0975ab2f2ee60ec73d
VHall1 <victorhallpsn@hotmail.com>
2023-04-11 20:14:46 +0100

    feat: add auth routes to router

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
-  resources :groups
-  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  resources :groups
+  resources :users
+
+  namespace :auth do
+    post :login
+    post :register
+  end
+
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "users#index"
 end
