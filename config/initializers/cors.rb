c5d1793e53042b6fc1fdfb2d2dfeb9a726e4d21c
VHall1 <victorhallpsn@hotmail.com>
2023-04-11 20:59:57 +0100

    fix: allow cors credentials

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,10 +7,9 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "example.com"
-
     resource "*",
       headers: :any,
+      credentials: true,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
