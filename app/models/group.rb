cf4734c29c64456f86bfa64f38932729e7d151ed
VHall1 <victorhallpsn@hotmail.com>
2023-04-11 20:16:39 +0100

    feat: validate group name

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,6 @@
 class Group < ApplicationRecord
   has_many :users, through: :user_groups
   has_many :user_groups
+
+  validates :name, presence: true, length: { maximum: 50 }, uniqueness: true
 end
