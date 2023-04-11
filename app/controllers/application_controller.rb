cf7432f80842483dcd86729e862b3aea0fc19b87
VHall1 <victorhallpsn@hotmail.com>
2023-04-11 11:58:37 +0100

    feat: stub application controller and require_admin helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  private
+
+  def require_admin!
+    return if current_user&.admin?
+
+    render json: { error: 'Unauthorized' }, status: :unauthorized
+  end
 end
