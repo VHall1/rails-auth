fc028431b4308c0f2b7169af74920940269f4839
VHall1 <victorhallpsn@hotmail.com>
2023-04-11 12:04:08 +0100

    refactor: use rails password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,8 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
                     uniqueness: true
-  validates :password, presence: true, length: { minimum: 6 }
 
+  # https://api.rubyonrails.org/v7.0.4/classes/ActiveModel/SecurePassword/ClassMethods.html
   has_secure_password
 
   def downcase_email
