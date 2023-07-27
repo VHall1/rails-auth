dcfbf7442fb31fb8ee8fd345fe48fd2eabca4f96
VHall1 <victorhallpsn@hotmail.com>
2023-07-27 18:08:42 +0100

    formatting

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,9 +7,9 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    resource "*",
-      headers: :any,
-      credentials: true,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    resource '*',
+             headers: :any,
+             credentials: true,
+             methods: %i[get post put patch delete options head]
   end
 end
