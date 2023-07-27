6ac06e6c71aa7da0ccbe945ef8fc3bf6f17d3498
VHall1 <victorhallpsn@hotmail.com>
2023-07-27 18:08:33 +0100

    disable session store

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,5 @@ module RailsAuth
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    config.middleware.use config.session_store, config.session_options
   end
 end
