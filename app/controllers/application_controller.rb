23270a0182da526cf77e9ba70a28fd5c857fb9b7
VHall1 <victorhallpsn@hotmail.com>
2023-07-28 14:05:52 +0100

    add record_not_found helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::API
   include AuthHelpers
+
+  private
+
+  def record_not_found(model)
+    render json: { errors: { model.to_sym => ['not found'] } }, status: :not_found
+  end
 end
