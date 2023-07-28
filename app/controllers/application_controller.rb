ecb74d05a148df633b4146e4153a3a0c24bf17c9
VHall1 <victorhallpsn@hotmail.com>
2023-07-28 14:12:06 +0100

    tweak error message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::API
   private
 
   def record_not_found(model)
-    render json: { errors: { model.to_sym => ['not found'] } }, status: :not_found
+    render json: { errors: { model.to_sym => ['must exist'] } }, status: :not_found
   end
 end
