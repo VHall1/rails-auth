7de7548c18084ef09986222691ec4180e83d2f18
VHall1 <victorhallpsn@hotmail.com>
2023-08-02 01:41:06 +0100

    remove old method from before_action list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :require_admin!
-  before_action :set_user, only: %i[ show update destroy assign_group groups ]
+  before_action :set_user, only: %i[show update destroy]
 
   # GET /users
   def index
