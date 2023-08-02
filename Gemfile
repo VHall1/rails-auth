3b2cd06329d90f2f5c003c1db640afc59b5fb823
VHall1 <victorhallpsn@hotmail.com>
2023-08-02 12:09:54 +0100

    add coverage report

--- Gemfile
+++ Gemfile
@@ -18,6 +18,7 @@ group :development, :test do
   gem "debug", platforms: %i[mri mingw x64_mingw]
   gem 'factory_bot_rails'
   gem 'rspec-rails', '~> 6.0.0'
+  gem 'simplecov', require: false
 end
 
 group :development do
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,8 +5,9 @@ require_relative '../config/environment'
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
-require 'factory_bot'
 # Add additional requires below this line. Rails is not loaded until this point!
+require 'factory_bot'
+require 'simplecov'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -63,4 +64,6 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  SimpleCov.start 'rails'
 end
